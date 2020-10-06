cbaa0704abe3212e6c7397774f9f0ce67e7516af
Toke Høiland-Jørgensen <toke@toke.dk>
2020-10-06 16:10:25 +0200

    encap-forward: Run ping and tcpdump as part of test setup
    Signed-off-by: Toke Høiland-Jørgensen <toke@toke.dk>

--- encap-forward/setup-test.sh
+++ encap-forward/setup-test.sh
@@ -17,7 +17,9 @@ ip a add dev bpf-encap2 fc00:dead:cafe:2::1/64
 $TESTENV exec ip link set dev veth1 up
 $TESTENV exec ip a add 10.11.2.2/24 dev veth1
 $TESTENV exec ip a add fc00:dead:cafe:2::2/64 dev veth1
-$TESTENV exec -- sysctl -w net.ipv4.ip_forward=1 net.ipv6.conf.all.forwarding=1 net.ipv4.conf.veth0.rp_filter=2 net.ipv4.conf.veth0.accept_local=1
+$TESTENV exec -- sysctl -w net.ipv4.ip_forward=1 net.ipv6.conf.all.forwarding=1 net.ipv4.conf.veth0.rp_filter=2 net.ipv4.conf.veth0.accept_local=0
 #$TESTENV exec -- ip link set dev veth0 xdp object xdp_encap.o
 $TESTENV exec -- tc qdisc add dev veth0 clsact
 $TESTENV exec -- tc filter add dev veth0 ingress bpf da obj tc_bpf_encap.o
+ping fc00:dead:cafe:1::2 &
+tcpdump -expni bpf-encap2
