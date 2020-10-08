71e076b08c74055d21f284125bc3710aceb85cca
Toke Høiland-Jørgensen <toke@toke.dk>
2020-10-08 16:02:16 +0200

    encap-forward: Use fib lookup + bpf_redirect() and bpf_redirect_neigh()
    Only for IPv4, and requires bpf-next + a modification to bpf_fib_lookup()
    to work.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@toke.dk>

--- encap-forward/tc_bpf_encap.c
+++ encap-forward/tc_bpf_encap.c
@@ -12,11 +12,16 @@
 #define ENCAP_TYPE BPF_F_ADJ_ROOM_ENCAP_L3_IPV4
 #endif
 
+#define AF_INET 2
+
 SEC("classifier") int tc_encap(struct __sk_buff *skb)
 {
+	struct bpf_fib_lookup fib_params = {};
 	volatile void *data, *data_end;
 	size_t offset = OFFSET;
 	int ret = BPF_DROP;
+	struct ethhdr *eth;
+	struct iphdr *iph;
 
 	if (bpf_skb_adjust_room(skb, offset, BPF_ADJ_ROOM_MAC, ENCAP_TYPE))
 		goto out;
@@ -29,8 +34,33 @@ SEC("classifier") int tc_encap(struct __sk_buff *skb)
 #else
 	encap_ipv4(data, data_end);
 
-	/* proposed new helper for skipping source validation:
-	bpf_skb_set_source_valid(skb, 1); */
+	eth = (void *)data;
+	iph = (void *)(eth +1);
+	if (iph +1 > data_end)
+		goto out;
+
+	eth->h_proto = bpf_htons(ETH_P_IP);
+
+	fib_params.family = AF_INET;
+	fib_params.tos = iph->tos;
+	fib_params.l4_protocol = iph->protocol;
+	fib_params.sport = 0;
+	fib_params.dport = 0;
+	fib_params.tot_len = bpf_ntohs(iph->tot_len);
+	fib_params.ipv4_src = iph->saddr;
+	fib_params.ipv4_dst = iph->daddr;
+	fib_params.ifindex = skb->ingress_ifindex;
+
+	ret = bpf_fib_lookup(skb, &fib_params, sizeof(fib_params), 0);
+	if (ret == BPF_FIB_LKUP_RET_SUCCESS) {
+		__builtin_memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
+		__builtin_memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
+
+		return bpf_redirect(fib_params.ifindex, 0);
+	} else if (ret == BPF_FIB_LKUP_RET_NO_NEIGH) {
+		return bpf_redirect_neigh(fib_params.ifindex, 0);
+	}
+
 #endif
 
 	ret = BPF_OK;
