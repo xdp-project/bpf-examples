3969089c646e8f68bac7f99dddbeff8276907795
Jesper Dangaard Brouer <brouer@redhat.com>
2020-11-14 13:53:49 +0100

    traffic-pacing-edt/tc_fq_pacer.sh: Add doc explaining
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- traffic-pacing-edt/tc_fq_pacer.sh
+++ traffic-pacing-edt/tc_fq_pacer.sh
@@ -2,6 +2,10 @@
 #
 # Loading FQ pacing qdisc in multi-queue MQ setup to avoid root qdisc lock.
 #
+# The FQ pacing qdisc is doing all the work of pacing packet out according to
+# the EDT (Earliest Departure Time) future timestamps set by our BPF-prog that
+# runs a TC-egress hook.
+#
 # Author: Jesper Dangaaard Brouer <netoptimizer@brouer.com>
 # License: GPLv2
 #
@@ -30,6 +34,7 @@ tc qdisc replace dev $DEV root handle 7FFF: mq
 # Add FQ-pacer qdisc on each NIC avail TX-queue
 i=0
 for dir in /sys/class/net/$DEV/queues/tx-*; do
+    # Details: cause-off-by-one, as tx-0 becomes handle 1:
     ((i++)) || true
     tc qdisc add dev $DEV parent 7FFF:$i handle $i: fq
 done
