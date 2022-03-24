7715df83eeabea3a36fc6180b31e1b38cc6eb0eb
Jesper Dangaard Brouer <brouer@redhat.com>
2022-03-24 12:25:07 +0100

    AF_XDP-interaction: Add BTF decoding code to btf_unit_test
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- AF_XDP-interaction/btf_unit_test.c
+++ AF_XDP-interaction/btf_unit_test.c
@@ -13,6 +13,8 @@
 
 #include "lib_xsk_extend.h"
 
+static int verbose = 1;
+
 /* Exit return codes - can be used by scripts looking at exit code */
 #define EXIT_OK             0 /* == EXIT_SUCCESS (stdlib.h) man exit(3) */
 #define EXIT_FAIL           1 /* == EXIT_FAILURE (stdlib.h) man exit(3) */
@@ -36,14 +38,100 @@ struct bpf_object *load_bpf_object() {
 	return obj;
 }
 
+/**
+ * BTF setup XDP-hints
+ * -------------------
+ * Setup the data structures for accessing the XDP-hints provided by
+ * kernel side BPF-prog via decoding BTF-info provided in BPF
+ * ELF-object file.
+ */
+
+/* This struct BTF mirrors kernel-side struct xdp_hints_rx_time */
+struct xdp_hints_rx_time {
+	__u32 btf_type_id; /* cached xsk_btf__btf_type_id(xbi) */
+	struct xsk_btf_info *xbi;
+	struct xsk_btf_member rx_ktime;
+	struct xsk_btf_member xdp_rx_cpu;
+} xdp_hints_rx_time = { 0 };
+
+/* This struct BTF mirrors kernel-side struct xdp_hints_mark */
+struct xdp_hints_mark {
+	__u32 btf_type_id; /* cached xsk_btf__btf_type_id(xbi) */
+	struct xsk_btf_info *xbi;
+	struct xsk_btf_member mark;
+} xdp_hints_mark = { 0 };
+
+struct xsk_btf_info *setup_btf_info(struct btf *btf,
+				    const char *struct_name)
+{
+	struct xsk_btf_info *xbi = NULL;
+	int err;
+
+	err = xsk_btf__init_xdp_hint(btf, struct_name, &xbi);
+	if (err) {
+		fprintf(stderr, "WARN(%d): Cannot BTF find struct:%s\n",
+			err, struct_name);
+		return NULL;
+	}
+
+	if (!xsk_btf__has_field("btf_id", xbi)) {
+		fprintf(stderr, "ERR: %s doesn't contain member btf_id\n",
+			struct_name);
+		xsk_btf__free_xdp_hint(xbi);
+		return NULL;
+	}
+
+	if (verbose)
+		printf("Setup BTF based XDP hints for struct: %s\n",
+		       struct_name);
+
+	return xbi;
+}
+
+int init_btf_info_via_bpf_object(struct bpf_object *bpf_obj)
+{
+	struct btf *btf = bpf_object__btf(bpf_obj);
+	struct xsk_btf_info *xbi;
+
+	xbi = setup_btf_info(btf, "xdp_hints_rx_time");
+	if (xbi) {
+		/* Lookup info on required member "rx_ktime" */
+		if (!xsk_btf__field_member("rx_ktime", xbi,
+					   &xdp_hints_rx_time.rx_ktime))
+			return -EBADSLT;
+		if (!xsk_btf__field_member("xdp_rx_cpu", xbi,
+					   &xdp_hints_rx_time.xdp_rx_cpu))
+			return -EBADSLT;
+		xdp_hints_rx_time.btf_type_id = xsk_btf__btf_type_id(xbi);
+		xdp_hints_rx_time.xbi = xbi;
+	}
+
+	xbi = setup_btf_info(btf, "xdp_hints_mark");
+	if (xbi) {
+		if (!xsk_btf__field_member("mark", xbi, &xdp_hints_mark.mark))
+			return -EBADSLT;
+		xdp_hints_mark.btf_type_id = xsk_btf__btf_type_id(xbi);
+		xdp_hints_mark.xbi = xbi;
+	}
+
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	struct bpf_object *bpf_obj;
-	// int err = 0;
+	int err = 0;
 
 	bpf_obj = load_bpf_object();
 	if (!bpf_obj)
 		return EXIT_FAIL_BPF;
+
+	err = init_btf_info_via_bpf_object(bpf_obj);
+	if (err) {
+		if (verbose)
+			printf("ERR(%d): Failed loading BTF info", err);
+		return EXIT_FAIL_BTF;
+	}
 
 	return EXIT_OK;
 }
