da3c568506f879696665425aad7868bc3e28ad15
Simon Sundberg <simon.sundberg@kau.se>
2022-04-22 17:59:58 +0200

    pping: Add netem config to ping test
    Add some simple automated setup and teardown of a netem qdisc to the
    ping accuracy test in order to get a more varied RTT.
    
    Signed-off-by: Simon Sundberg <simon.sundberg@kau.se>

--- pping/measurements/ping_test.sh
+++ pping/measurements/ping_test.sh
@@ -6,11 +6,16 @@
 # one can compare them and correlate them perfectly as each will generate
 # exactly one report per packet
 
-MACHINE=${MACHINE:-"Simon-RM-2"}
-TARGET=${TARGET:-"172.16.24.31"}
-IFACE=${IFACE:-"ens192"}
+MACHINE=${MACHINE:-"testbed-lenovo"}
+NETEM_MACHINE=${NETEM_MACHINE:-"testbed-40g-02"}
+TARGET=${TARGET:-"10.70.2.2"}
+IFACE=${IFACE:-"ens3f1"}
+NETEM_IFACE=${NETEM_IFACE:-"enp1s0f1np1"}
 PING_FLAGS=${PING_FLAGS:-"-Dn -i 0.01 -c 1000"}
 EPPING_FLAGS=${EPPING_FLAGS:-"-Cf -r 0 -F ppviz"}
+NETEM_ARGS="delay 100ms 10ms 95%"
+
+export MPLBACKEND=agg
 
 start_epping() {
     local machine=$1
@@ -33,13 +38,30 @@ stop_epping() {
     ssh $machine "sudo pkill -f 'pping -i $iface'"
 }
 
+setup_netem() {
+    local machine=$1
+    local iface=${2:-$NETEM_IFACE}
+    local netem_args=${3:-$NETEM_ARGS}
+
+    echo "${machine}: Setting up netem $netem_args on dev $iface"
+    ssh $machine "sudo tc qdisc add dev $iface root netem $netem_args"
+}
+
+teardown_netem() {
+    local machine=$1
+    local iface=${2:-$NETEM_IFACE}
+
+    echo "${machine}: Removing netem from dev ${iface}"
+    ssh $machine "sudo tc qdisc del dev $iface root netem"
+    }
+
 run_ping() {
     local machine=$1
     local target=$2
     local save_path=$3
     local ping_flags=${4:-$PING_FLAGS}
 
-    echo "Pinging ${target} from ${machine}..."
+    echo "${machine}: Pinging $target with args ${ping_flags}..."
     ssh $machine "mkdir -p $save_path; sudo ping $target $ping_flags > ${save_path}/ping.out 2> ${save_path}/ping.err"
 }
 
@@ -69,9 +91,17 @@ for (( i = 1; i <= $n_runs; i++)); do
 	save_path=$base_path
     fi
 
+    if [[ -n $NETEM_MACHINE && -n $NETEM_IFACE ]]; then
+	setup_netem $NETEM_MACHINE $NETEM_IFACE
+    fi
+
     start_epping $MACHINE $save_path
     run_ping $MACHINE $TARGET $save_path
     stop_epping $MACHINE
     copy_back_results $MACHINE $save_path "compress"
+
+    if [[ -n $NETEM_MACHINE && -n $NETEM_IFACE ]]; then
+	teardown_netem $NETEM_MACHINE $NETEM_IFACE
+    fi
 done
 
